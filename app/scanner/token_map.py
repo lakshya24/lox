c577a86a1f900ec0a62bd0735cafda657fa72f4c
Lakshya garg <lakshya@decanaria.com>
2024-07-13 19:41:14 -0400

    tokenize and scan other single char

--- app/scanner/token_map.py
+++ app/scanner/token_map.py
@@ -9,11 +9,26 @@ class TokenType(Enum):
     LEFT_BRACE = auto()
     RIGHT_BRACE = auto()
     EOF = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
 
 
 TOKEN_MAP: Dict[str, TokenType] = {
+    # Single Char Tokens
     "(": TokenType.LEFT_PAREN,
     ")": TokenType.RIGHT_PAREN,
     "{": TokenType.LEFT_BRACE,
     "}": TokenType.RIGHT_BRACE,
+    ",": TokenType.COMMA,
+    ".": TokenType.DOT,
+    "-": TokenType.MINUS,
+    "+": TokenType.PLUS,
+    ";": TokenType.SEMICOLON,
+    "/": TokenType.SLASH,
+    "*": TokenType.STAR,
 }
