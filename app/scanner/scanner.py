c32fa2fa4ca14d7b32f00b94a5b4095b48e2c103
Lakshya garg <lakshya@decanaria.com>
2024-07-14 14:28:40 -0400

    Support for Assignment & equality Operators

--- app/scanner/scanner.py
+++ app/scanner/scanner.py
@@ -1,7 +1,7 @@
 from typing import List
 from app.lox import Lox
 from app.scanner.token import Token
-from app.scanner.token_map import TOKEN_MAP, TokenType
+from app.scanner.token_map import CONDITION_EQUAL_MAP, TOKEN_MAP, TokenType
 
 
 class Scanner:
@@ -32,6 +32,21 @@ class Scanner:
         c: str = self.advance()
         if token_type := TOKEN_MAP.get(c):
             text = self.source[self.start : self.current]
+            if token_type in CONDITION_EQUAL_MAP.keys():
+                token_type = (
+                    CONDITION_EQUAL_MAP[token_type]
+                    if self.is_match_equal("=")
+                    else token_type
+                )
+                text = self.source[self.start : self.current]
             self.tokens.append(Token(token_type, text, None, self.line))
         else:
             self.lox.error(self.line, f"Unexpected character: {c}")
+
+    def is_match_equal(self, expected):
+        if self.is_end_of_content():
+            return False
+        if self.source[self.current] != expected:
+            return False
+        self.current += 1
+        return True
--- app/scanner/token_map.py
+++ app/scanner/token_map.py
@@ -16,6 +16,12 @@ class TokenType(Enum):
     SEMICOLON = auto()
     SLASH = auto()
     STAR = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
 
 
 TOKEN_MAP: Dict[str, TokenType] = {
@@ -31,4 +37,16 @@ TOKEN_MAP: Dict[str, TokenType] = {
     ";": TokenType.SEMICOLON,
     "/": TokenType.SLASH,
     "*": TokenType.STAR,
+    "=": TokenType.EQUAL,
+    "==": TokenType.EQUAL_EQUAL,
+    "<": TokenType.LESS,
+    "<=": TokenType.LESS_EQUAL,
+    ">": TokenType.GREATER,
+    ">=": TokenType.GREATER_EQUAL,
+}
+
+CONDITION_EQUAL_MAP: Dict[TokenType, TokenType] = {
+    TokenType.LESS: TokenType.LESS_EQUAL,
+    TokenType.GREATER: TokenType.GREATER_EQUAL,
+    TokenType.EQUAL: TokenType.EQUAL_EQUAL,
 }
